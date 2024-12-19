8fe961f121a06b7d13cc319974ce8b35cd77a85a
Julian Bilcke <julian.bilcke@huggingface.co>
2024-12-19 18:45:03 +0100

    debugging..

---
+++ varnish/__init__.py
@@ -0,0 +1,24 @@
+from varnish import (
+    OutputType,
+    ProcessingStage,
+    VideoMetadata,
+    MMAudioConfig,
+    ProcessingProgress,
+    VideoProcessor,
+    VarnishResult,
+    Varnish
+)
+
+# Importing with `from varnish import *` will only import these names
+__all__ = [
+    "__version__",
+    "PipelineImageInput",
+    "OutputType",
+    "ProcessingStage",
+    "VideoMetadata",
+    "MMAudioConfig",
+    "ProcessingProgress",
+    "VideoProcessor",
+    "VarnishResult",
+    "Varnish"
+]
