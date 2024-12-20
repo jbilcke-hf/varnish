0ff55896b8b9f8ecee1a75d6d7d3e180e81ae3e8
Julian Bilcke <julian.bilcke@huggingface.co>
2024-12-20 01:16:45 +0100

    fix

--- varnish/rife_model.py
+++ varnish/rife_model.py
@@ -8,7 +8,7 @@ import logging
 import skvideo.io
 from .rife.RIFE_HDv3 import Model
 
-import .utils
+from . import utils
 
 logger = logging.getLogger(__name__)
 device = "cuda" if torch.cuda.is_available() else "cpu"
